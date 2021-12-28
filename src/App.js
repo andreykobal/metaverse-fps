56caafc15e710b66e7c1a0627c4f9d6c4f5bc569
Andreas Kobal <kobalandreas@gmail.com>
2021-12-28 03:46:32 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Unity, { UnityContent } from "react-unity-webgl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const unityContent = new UnityContent(
+  "build/asdasddas.json",
+  "build/UnityLoader.js"
+);
 
-export default App;
+const App = () => {
+  return <Unity unityContent={unityContent} height="100%" width="950px" />
+  ;
+};
+
+export default App
